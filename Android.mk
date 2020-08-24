e4f45930b2465f78510364edd29c6c282b1f9e0f
Turtle <46635000+turtleletortue@users.noreply.github.com>
2020-08-23 20:12:18 -0400

    x15: Use x18 vendor

--- Android.mk
+++ Android.mk
@@ -61,4 +61,50 @@ include $(BUILD_PREBUILT)
 
 endif
 
+ifeq ($(TARGET_DEVICE),x15)
+$(info [x15] Copying DP Framework proprietary blobs)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libaudiocustparam
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = mtk
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libaudiocustparam.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libaudiocustparam
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = mtk
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 64
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libaudiocustparam.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = mtk
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 64
+LOCAL_SRC_FILES_64 = proprietary/vendor/lib64/libdpframework.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = mtk
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libdpframework.so
+include $(BUILD_PREBUILT)
+
+endif
+
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
